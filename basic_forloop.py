17a1d00e0bac9f878c963f7762c0d643d46c90bd
manisha <manisha.verma23@gmail.com>
2023-01-01 18:45:44 +0530

    basic forloop example for jenkins pipeline

---
+++ basic_forloop.py
@@ -0,0 +1,7 @@
+# for loop
+
+fruits = ["Apple","Orange","Guava","Grapes","Banana"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")  #will be printed after every fruit name
+print("Have these fruits daily") # This line will be printed once after all fruit names
